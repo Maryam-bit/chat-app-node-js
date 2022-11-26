c4945c70e57f9f4a54566a434938163f54f2d295
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-26 23:07:37 +0500

    rendering messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,38 @@
 
 const socket = io()
+// elements
+const messageForm = document.querySelector("#message-form")
+const messageFormInput = document.querySelector("input")
+const messageFormButton = document.querySelector("button")
+const sendLocationButton = document.querySelector("#send-location")
+const messages = document.querySelector("#messages")
+
+// templates
+const messageTemplate = document.querySelector("#message-template").innerHTML
 
 // server (emit) -> client (receive) --acknowledgement --> server
 // client (emit) -> server (receive) --acknowledgement --> client
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+
+socket.on("message", (message) => {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    messages.insertAdjacentHTML("beforeend", html)
+})
+
+messageForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
+    messageFormButton.setAttribute("disabled", true)
     // const message = document.querySelector("input").value
     const message = e.target.elements.message.value
 
     socket.emit("sendMessage", message, (error) => {
+        messageFormButton.removeAttribute("disabled")
+        messageFormInput.value = ""
+        messageFormInput.focus()
         if(error) {
             return console.log(error)
         }
@@ -18,17 +40,16 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
     })
 })
 
-socket.on("message", (message) => {
-    console.log(message)
-})
 
-document.querySelector("#send-location").addEventListener("click", () => {
+sendLocationButton.addEventListener("click", () => {
     if(!navigator.geolocation) {
         return alert("Geolocation is not supported by your browser")
     }
+    sendLocationButton.setAttribute("disabled", true)
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit("sendLocation", {latitude: position.coords.latitude, longitude: position.coords.longitude}, (message) => {
             console.log(message)
+            sendLocationButton.removeAttribute("disabled")
         })
     }) 
 })
