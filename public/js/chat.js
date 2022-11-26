aae679f88331f72d18bd501f926938b170761f19
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-26 22:23:06 +0500

    feat: events acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,21 @@
 
 const socket = io()
 
+// server (emit) -> client (receive) --acknowledgement --> server
+// client (emit) -> server (receive) --acknowledgement --> client
+
 document.querySelector("#message-form").addEventListener("submit", (e) => {
     e.preventDefault();
 
     // const message = document.querySelector("input").value
     const message = e.target.elements.message.value
 
-    socket.emit("sendMessage", message)
+    socket.emit("sendMessage", message, (error) => {
+        if(error) {
+            return console.log(error)
+        }
+        console.log("Message delivered!")
+    })
 })
 
 socket.on("message", (message) => {
@@ -19,6 +27,8 @@ document.querySelector("#send-location").addEventListener("click", () => {
         return alert("Geolocation is not supported by your browser")
     }
     navigator.geolocation.getCurrentPosition((position) => {
-        socket.emit("sendLocation", {latitude: position.coords.latitude, longitude: position.coords.longitude})
+        socket.emit("sendLocation", {latitude: position.coords.latitude, longitude: position.coords.longitude}, (message) => {
+            console.log(message)
+        })
     }) 
 })
