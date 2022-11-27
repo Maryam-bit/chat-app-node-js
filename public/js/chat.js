9c6b8f680267b0b1031ad4f7f87368070239b881
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-27 19:02:33 +0500

    feat: showing list of users and room name

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ const messages = document.querySelector("#messages")
 // templates
 const messageTemplate = document.querySelector("#message-template").innerHTML
 const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML
 
 // Options
 const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true})
@@ -38,6 +39,14 @@ socket.on("locationMessage", (message) => {
     messages.insertAdjacentHTML("beforeend", html)
 })
 
+socket.on("roomData", ({room, users}) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector("#sidebar").innerHTML = html
+
+})
 messageForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
--- src/index.js
+++ src/index.js
@@ -40,6 +40,11 @@ io.on("connection", (socket) => {
         socket.join(user.room)
         socket.emit("message", generateMessage("Admin", "Welcome!"))
         socket.broadcast.to(user.room).emit("message", generateMessage("Admin", `${user.username} has joined!`))
+
+        io.to(user.room).emit("roomData", {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
         callback()
     })
 
@@ -65,6 +70,10 @@ io.on("connection", (socket) => {
         const user = removeUser(socket.id)
         if(user) {
             io.to(user.room).emit("message", generateMessage("Admin", `${user.username} has left!`))
+            io.to(user.room).emit("roomData", {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
         }
     })
 })
