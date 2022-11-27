e976d1a87dd97f873147970042d0d8642b31b544
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-27 13:49:45 +0500

    feat: added timestamps

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require("http")
 const express = require("express")
 const socketio = require("socket.io")
 const Filter = require("bad-words")
+const { generateMessage, generateLocationMessage } = require("./utils/messages")
 
 const app = express()
 const server = http.createServer(app)
@@ -26,8 +27,8 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 io.on("connection", (socket) => {
     console.log("new web socket connection")
 
-    socket.emit("message", "Welcome!")
-    socket.broadcast.emit("message", "A new user has joined")
+    socket.emit("message", generateMessage("Welcome!"))
+    socket.broadcast.emit("message", generateMessage("A new user has joined"))
 
     socket.on("sendMessage", (message, callback) => {
         const filter = new Filter()
@@ -36,17 +37,17 @@ io.on("connection", (socket) => {
             return callback("profanity is not allowed!")
         }
 
-        io.emit("message", message)
+        io.emit("message", generateMessage(message))
         callback()
     })
 
     socket.on("sendLocation", (coords, callback) => {
-        io.emit("locationMessage", `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit("locationMessage", generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback("Location Shared!")
     })
 
     socket.on("disconnect", () => {
-        io.emit("message", "A user has left!")
+        io.emit("message", generateMessage("A user has left!"))
     })
 })
 
